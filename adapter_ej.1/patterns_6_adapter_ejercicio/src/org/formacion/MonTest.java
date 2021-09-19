fd251795d7ebad2dd14af22723b8b673a1603a91
Azucena17 <63064440+Azucena17@users.noreply.github.com>
2021-09-19 04:19:51 -0600

    Add files via upload

---
+++ adapter_ej.1/patterns_6_adapter_ejercicio/src/org/formacion/MonTest.java
@@ -0,0 +1,14 @@
+package org.formacion;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MonTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
